f8d92e34b4d948178482815f7fcfa51aa0293b5f
haarp <main.haarp@gmail.com>
2026-04-04 23:47:57 +0200

    update bogusintel for MKL 2025.0 (https://danieldk.eu/Software/Misc/Intel-MKL-on-AMD-Zen#addendum)

--- bin/bogusintel.c
+++ bin/bogusintel.c
@@ -1,8 +1,14 @@
 // Disable Cripple-AMD CPU dispatcher in Intel Math Kernel Library
 // https://en.wikipedia.org/wiki/Math_Kernel_Library#Performance
-// compile with `gcc -shared -o bogusintel.so bogusintel.c`
+// compile with `gcc -shared -fPIC -o bogusintel.so bogusintel.c`
 // LD_PRELOAD=bogusintel.so yourprogram
 
 int mkl_serv_intel_cpu_true() {
 	return 1;
+}
+
+typedef int (*fakeintel_fptr)(void);
+
+fakeintel_fptr mkl_serv_get_cpu_true() {
+	return &mkl_serv_intel_cpu_true;
 }
